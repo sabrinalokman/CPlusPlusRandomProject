c638c254eb2d4fcffe642f06424232c917620cc4
Sabrina <sabrina.lokman@mail.utoronto.ca>
2020-09-26 13:43:43 -0400

    Initial Commit

---
+++ Cube/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cube
+//
+//  Created by Sabrina Lokman on 2020-09-26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
