c71a12aaafa64c87c6230f5e290a16b5ba6cd4fa
Sabrina Lokman <35489261+sabrinalokman@users.noreply.github.com>
2020-11-08 20:36:28 -0500

    Add question

--- K-MessedArraySort/k-Messed.cpp
+++ K-MessedArraySort/k-Messed.cpp
@@ -4,6 +4,21 @@
 
 using namespace std;
 
+/*
+K-Messed Array Sort
+Given an array of integers arr where each element is at most k places away from its sorted position, code an efficient function sortKMessedArray that sorts arr. For instance, for an input array of size 10 and k = 2, an element belonging to index 6 in the sorted array will be located at either index 4, 5, 6, 7 or 8 in the input array.
+
+Analyze the time and space complexities of your solution.
+
+Example:
+
+input:  arr = [1, 4, 5, 2, 3, 7, 8, 6, 10, 9], k = 2
+
+output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+*/
+
+//TO DO: FIX ALGO TO PASS THE LAST TEST CASE
+
 vector<int> sortKMessedArray(vector<int> arr, int k ) 
 {
   // your code goes here
@@ -20,6 +35,8 @@ vector<int> sortKMessedArray(vector<int> arr, int k )
   for (int i = 0 ; i < arr.size(); i++) {
   	std::cout << arr[i] << " ";
   }
+
+  std::cout << "\n";
   return arr;
 }
 
@@ -27,5 +44,23 @@ int main() {
   vector<int> arr{4, 5, 2, 3, 7, 8, 6, 10, 9};
   int k = 2;
   sortKMessedArray(arr, k);
+  
+  vector<int> arr1{1,0}; k = 1;
+  sortKMessedArray(arr1, k);
+
+  vector<int> arr2{1,0,3,2}; k = 1;
+  sortKMessedArray(arr2, k);
+
+  vector<int> arr3{1,0,3,2,4,5,7,6,8}; k = 1;
+  sortKMessedArray(arr3, k);
+
+  vector<int> arr4{1,2,3,4,5,6,7,8,9,10}; k = 2;
+  sortKMessedArray(arr4, k);
+
+  vector<int> arr5{6,1,4,11,2,0,3,7,10,5,8,9}; k = 6;
+  sortKMessedArray(arr5, k);
+
+
+
   return 0;
 }
